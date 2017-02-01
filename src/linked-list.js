c8bf062da2aa7afbdeab0b8330b45d1564980b36
Alesya <saauser77@mail.ru>
2017-02-01 23:17:59 +0300

    doubly-linked-list/feachure: constructor finished

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,14 +1,33 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
-    append(data) {}//assign any nodes to this._head and this._tail if list is ampty
+    constructor() {
+      this.length = 0;
+    }
+    append(data) {//assign any nodes to this._head and this._tail if list is ampty
                   //adds new data in the end of the list
+if(this.length==0){
+this._head = new Node;
+this._tail = new Node;
+}
+this.length++;
+//this.tail=this.head;
+//this.head=data;
+
+//this._tail=this.data;
+
+}
+
+
+
 
 
     head() {//returns data from this.head
+return this.head;
 }
     tail() {//returns data from this.tail
+
+return this.tail;
 }
     at(index) {//returns Node.data by index
 }
